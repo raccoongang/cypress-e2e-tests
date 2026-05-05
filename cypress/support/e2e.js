d1dfc32690df298f9950bf10b1a5d55bbe3dc7f2
Danyal Faheem <danyal.faheem@edly.io>
2026-05-05 18:27:06 +0500

    fix: import register from cypress/grep
    From cypress/grep v5.0.0 onwards, the import has been updated as a breaking change.
    
    ref: https://github.com/cypress-io/cypress/blob/develop/npm/grep/CHANGELOG.md#cypressgrep-v500-2025-09-19
    ref: https://github.com/cypress-io/cypress/discussions/32641#discussioncomment-15763862
    
    The current cypress-e2e-tests are broken when run with node v22.22 due to this import change.

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -1,4 +1,4 @@
 import './commands'
-import registerCypressGrep from '@cypress/grep'
+import { register as registerCypressGrep } from '@cypress/grep';
 
-registerCypressGrep()
+registerCypressGrep();
